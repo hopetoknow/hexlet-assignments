0fa80b7b228f65ddac6793f0f7e02c50bd24f7d2
@hexlet/cli <support@hexlet.io>
2022-10-21 18:58:25 +0300

    submit java-web-ru/deploy

--- java-web-ru/deploy/src/main/java/exercise/servlet/CompaniesServlet.java
+++ java-web-ru/deploy/src/main/java/exercise/servlet/CompaniesServlet.java
@@ -1,14 +1,13 @@
 package exercise.servlet;
 
-import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
-import java.util.stream.Collectors;
+
 import static exercise.Data.getCompanies;
 
 public class CompaniesServlet extends HttpServlet {
@@ -19,24 +18,24 @@ public class CompaniesServlet extends HttpServlet {
                 throws IOException, ServletException {
 
         // BEGIN
-        List<String> companies = getCompanies();
-        String query = request.getQueryString();
-        String param = request.getParameter("search");
         PrintWriter out = response.getWriter();
+        List<String> companies = getCompanies();
 
-        if (query == null || "".equals(param)) {
-            out.println(String.join("\n", companies));
-        } else {
-            String filteredCompanies = companies.stream()
-                    .filter(company -> company.contains(param))
-                    .collect(Collectors.joining("\n"));
+        String searchString = request.getParameter("search") == null
+                ? ""
+                : request.getParameter("search");
 
-            if (filteredCompanies.length() > 0) {
-                out.println(filteredCompanies);
-            } else {
-                out.println("Companies not found");
-            }
+        List<String> filteredCompanies = companies
+                .stream()
+                .filter(company -> company.contains(searchString))
+                .toList();
+
+        if (filteredCompanies.isEmpty()) {
+            out.println("Companies not found");
+            return;
         }
+
+        filteredCompanies.forEach(out::println);
         // END
     }
 }
